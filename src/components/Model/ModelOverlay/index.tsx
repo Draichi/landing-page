e1628ea7a77005928749ca931a332ddeb73746b5
@Draichi <lucasdraichi@sabido.com>
2022-02-10 19:12:51 -0300

    feat: uses framer-motion to show/hide each section

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,4 +1,11 @@
-import { ReactNode, useCallback, useLayoutEffect, useState } from "react";
+import { useTransform } from "framer-motion";
+import {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react";
 import { CarModel } from "../ModelsContext";
 import useWrapperScroll from "../useWrapperScroll";
 
@@ -35,7 +42,22 @@ function ModelOverlay({ children, model }: ModelOverlayProps) {
 
   const { scrollY } = useWrapperScroll();
 
-  return <Container>{children}</Container>;
+  const sectionScrollProgress = useTransform(
+    scrollY,
+    (y) => (y - dimentions.offsetTop) / dimentions.offsetHeight
+  );
+
+  const opacity = useTransform(
+    sectionScrollProgress,
+    [-0.42, -0.05, 0.05, 0.42],
+    [0, 1, 1, 0]
+  );
+
+  const pointerEvents = useTransform(opacity, (value) =>
+    value > 0 ? "auto" : "none"
+  );
+
+  return <Container style={{ opacity, pointerEvents }}>{children}</Container>;
 }
 
 export default ModelOverlay;
