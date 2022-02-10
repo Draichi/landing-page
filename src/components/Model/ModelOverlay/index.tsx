fea6da44dbc277ebd44579dd6cabb744121e4a9e
@Draichi <lucasdraichi@sabido.com>
2022-02-09 22:24:34 -0300

    feat: model overlay scroll

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useCallback, useLayoutEffect, useState } from "react";
 import { CarModel } from "../ModelsContext";
 import useWrapperScroll from "../useWrapperScroll";
 
@@ -9,7 +9,30 @@ interface ModelOverlayProps {
   model: CarModel;
 }
 
-function ModelOverlay({ children }: ModelOverlayProps) {
+type SectionDimentions = Pick<HTMLDivElement, "offsetTop" | "offsetHeight">;
+
+function ModelOverlay({ children, model }: ModelOverlayProps) {
+  const getSectionDimentions = useCallback(() => {
+    return {
+      offsetTop: model.sectionRef.current?.offsetTop,
+      offsetHeight: model.sectionRef.current?.offsetHeight,
+    } as SectionDimentions;
+  }, [model.sectionRef]);
+
+  const [dimentions, setDimentions] = useState<SectionDimentions>(
+    getSectionDimentions()
+  );
+
+  useLayoutEffect(() => {
+    function onResize() {
+      window.requestAnimationFrame(() => setDimentions(getSectionDimentions()));
+    }
+
+    window.addEventListener("resize", onResize);
+
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
   const { scrollY } = useWrapperScroll();
 
   return <Container>{children}</Container>;
